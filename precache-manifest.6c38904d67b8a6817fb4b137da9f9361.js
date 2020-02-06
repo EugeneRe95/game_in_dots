bd8d9646964e91a9f0c63af321d133979486159a
Eugene Revutskiy <revytskiy.evgen@gmail.com>
2020-02-06 19:59:26 +0200

    Updates

---
+++ precache-manifest.6c38904d67b8a6817fb4b137da9f9361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d73fe42690392ae6683d91a5543647",
+    "url": "/game_in_dots/index.html"
+  },
+  {
+    "revision": "e48ae8915f487caadeae",
+    "url": "/game_in_dots/static/css/main.3737d604.chunk.css"
+  },
+  {
+    "revision": "6bae370ea37fed1c1457",
+    "url": "/game_in_dots/static/js/2.4b0faf23.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/game_in_dots/static/js/2.4b0faf23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48ae8915f487caadeae",
+    "url": "/game_in_dots/static/js/main.09fb0d2e.chunk.js"
+  },
+  {
+    "revision": "9dd27af138c13f664230",
+    "url": "/game_in_dots/static/js/runtime-main.0194ae06.js"
+  }
+]);
